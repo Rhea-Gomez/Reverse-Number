4438f74ff27a885c9f70f5dc43a1724fd4d38af4
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 01:35:29 +0530

    Assignment on Reverse of Number

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, remainder, reverse = 0, temp;
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            temp = num;
+            while (temp > 0)
+            {
+                remainder = temp % 10;
+                reverse = 10 * reverse + remainder;
+                temp = temp / 10;
+            }
+
+            Console.WriteLine("Reverse of " + num + " is " + reverse);
+        }
+    }
+}
